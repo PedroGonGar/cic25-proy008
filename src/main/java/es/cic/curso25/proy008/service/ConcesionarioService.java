1d43afc5f996e0fef0a93a7eceb8b6b55c72c7a7
noname <no@mail.no>
2025-07-23 20:09:39 +0200

    Añadido el código de ConcesionarioService

--- src/main/java/es/cic/curso25/proy008/service/ConcesionarioService.java
+++ src/main/java/es/cic/curso25/proy008/service/ConcesionarioService.java
@@ -1,5 +1,47 @@
 package es.cic.curso25.proy008.service;
 
-public class ConcesionarioService {
+import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.cic.curso25.proy008.model.Concesionario;
+import es.cic.curso25.proy008.repository.ConcesionarioRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ConcesionarioService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConcesionarioService.class);
+
+    @Autowired
+    private ConcesionarioRepository concesionarioRepository;
+
+    public Optional<Concesionario> get(Long id) {
+        LOGGER.info(String.format("Buscando el concesionario con id: %d", id));
+        return concesionarioRepository.findById(id);
+    }
+
+    public List<Concesionario> get() {
+        LOGGER.info("Buscando todos los concesionarios");
+        return concesionarioRepository.findAll();
+    }
+
+    public Concesionario create(Concesionario concesionario) {
+        LOGGER.info("Creando un concesionario");
+        return concesionarioRepository.save(concesionario);
+    }
+
+    public Concesionario update(Concesionario concesionario) {
+        LOGGER.info(String.format("Actualizando el concesionario con id: %d", concesionario.getId()));
+        return concesionarioRepository.save(concesionario);
+    }
+
+    public void delete(Long id) {
+        LOGGER.info("Eliminando concesionario con id: %d", id);
+        concesionarioRepository.deleteById(id);
+    }
 }
