fa2964411f4c2533b06b7d6d9d1326b8b2d06610
HeDichoQueNo <no@nomail.no>
2025-07-23 10:52:24 +0200

    Cambios en el main hugo 2

--- src/main/java/es/cic/curso25/proy008/exception/MotoException.java
+++ src/main/java/es/cic/curso25/proy008/exception/MotoException.java
@@ -1,5 +1,13 @@
 package es.cic.curso25.proy008.exception;
 
-public class MotoException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MotoException extends RuntimeException {
+
+    public MotoException(long id) {
+        super("No se ha encontrado una moto con el id " + id);
+    }
 
 }
