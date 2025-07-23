446276bea17dee457ae96b58bebb8a03d72aaa11
HeDichoQueNo <no@nomail.no>
2025-07-23 10:51:52 +0200

    Cambios hugo en el main 1

--- src/main/java/es/cic/curso25/proy008/model/Moto.java
+++ src/main/java/es/cic/curso25/proy008/model/Moto.java
@@ -1,5 +1,13 @@
 package es.cic.curso25.proy008.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
 public class Moto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/es/cic/curso25/proy008/repository/MotoRepository.java
+++ src/main/java/es/cic/curso25/proy008/repository/MotoRepository.java
@@ -1,5 +1,14 @@
 package es.cic.curso25.proy008.repository;
 
-public interface MotoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.cic.curso25.proy008.model.Moto;
+
+/*──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ * Clase Repository, mutada a una interfaz que hereda de JPAREPOSITORY Los métodos básicos
+ * de manipulación de BBDD
+ * ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ */
+public interface MotoRepository extends JpaRepository<Moto, Long> {
 
 }
